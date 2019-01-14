3d68ca6e30901e4d68dd28e5e6b2869f8680a3e5
IliaTaboo <iliataboo@gmail.com>
2019-01-14 19:32:27 +0200

    feat: add styles, routes, basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './core/auth.guard';
+import { HomePageComponent } from './ui/home-page/home-page.component';
+import { SigninPageComponent } from './ui/signin-page/signin-page.component';
+import { LoginPageComponent } from './ui/login-page/login-page.component';
+import { StartPageComponent } from './ui/start-page/start-page.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: 'signin', component: SigninPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: '', component: StartPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -4,14 +4,25 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SigninPageComponent } from './signin-page/signin-page.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
   declarations: [
     UserProfileComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SigninPageComponent,
+    StartPageComponent,
+    NavigationComponent,
   ],
   exports: [
     UserProfileComponent,
+    NavigationComponent,
   ]
 })
 export class UiModule { }
